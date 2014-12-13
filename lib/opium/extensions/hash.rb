a57292fda1d582a44e4b3111bf18b9ebdbf836d7
Joshua Bowers <joshua.bowers+code@gmail.com>
2014-12-13 10:57:42 -0800

    Cleaned up some of the redundancies in the hash extension. Also, made it behave in an indifferent access fashion for date/time conversions.

--- lib/opium/extensions/hash.rb
+++ lib/opium/extensions/hash.rb
@@ -5,31 +5,35 @@ class Hash
   end
   
   def to_datetime
-    validates_keys_present( '__type', 'iso' )
-    validate_key_equals( '__type', 'Date' )
-    self['iso'].to_datetime
+    retrieve_iso_key.to_datetime
   end
   
   def to_date
-    validates_keys_present( '__type', 'iso' )
-    validate_key_equals( '__type', 'Date' )
-    self['iso'].to_date
+    retrieve_iso_key.to_date
   end
   
   def to_time
-    validates_keys_present( '__type', 'iso' )
-    validate_key_equals( '__type', 'Date' )
-    self['iso'].to_time
+    retrieve_iso_key.to_time
   end
   
   private
   
+  def retrieve_iso_key
+    validates_keys_present( '__type', 'iso' )
+    validate_key_equals( '__type', 'Date' )
+    value_for_indifferent_key('iso')
+  end
+  
   def validates_keys_present(*expected_keys)
-    result = expected_keys - self.keys
+    result = expected_keys - self.keys.map(&:to_s)
     raise ArgumentError, "expected key(s): #{result}" unless result.empty?
   end
   
   def validate_key_equals( key, value )
-    raise ArgumentError, "conversion to Date/Time expectes a __type of Date" unless self[key] == value
+    raise ArgumentError, "conversion to Date/Time expectes a #{key} of #{value}" unless self[key] == value || self[key.to_sym] == value
+  end
+  
+  def value_for_indifferent_key( key )
+    self[key] || self[key.to_sym]
   end
 end
