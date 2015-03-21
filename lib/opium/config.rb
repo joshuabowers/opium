5ea2db1fa5fdc34c7c15c52f86f124f8d80ecaf9
Joshua Bowers <joshua.bowers+code@gmail.com>
2015-03-21 15:39:10 -0700

    Renaming configuration.rb to config.rb to make it more concise; introducing a spec to wrap it.

---
+++ lib/opium/config.rb
@@ -0,0 +1,7 @@
+module Opium
+  # with_configuration do
+  #   has :app_id, classes: String, default: 'abc123'
+  #   has :api_key, classes: String, default: 'abc123'
+  #   has :log_network_responses, default: false
+  # end
+end
---
+++ spec/opium/config_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Opium do
+  it { should respond_to(:configure, :config) }
+  
+  describe Opium::Config do
+  end
+end
