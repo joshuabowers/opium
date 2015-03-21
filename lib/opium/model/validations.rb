ced68a26ad8c4ca8406238e1f3e6d78f356bf6a9
Joshua Bowers <joshua.bowers+code@gmail.com>
2015-03-20 23:38:51 -0700

    Removing dead code, which is obsoleted by ActiveModel::Model.

--- lib/opium/model/validations.rb
+++
@@ -1,9 +0,0 @@
-module Opium
-  module Model
-    module Validations
-      extend ActiveSupport::Concern
-      
-      include ActiveModel::Validations
-    end
-  end
-end
--- spec/opium/model/naming_spec.rb
+++
@@ -1,8 +0,0 @@
-# require 'spec_helper'
-#
-# describe Opium::Model::Naming do
-#   let( :model ) { Class.new { include Opium::Model::Naming } }
-#   it 'should have #model_name' do
-#     model.should respond_to(:model_name)
-#   end
-# end
