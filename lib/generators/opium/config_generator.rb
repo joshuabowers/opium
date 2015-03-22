7a9a4cd42fa4485044ec6ff6edd7c916872fbf25
Joshua Bowers <joshua.bowers+code@gmail.com>
2015-03-21 19:27:00 -0700

    Attempting to reconfigure gem so that Rails will properly recognize the generators.

---
+++ lib/generators/opium/config_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators'
+
+module Opium
+  module Generators
+    class ConfigGenerator < ::Rails::Generators::Base
+      source_root File.expand_path( "../templates/", __FILE__ )
+      
+      desc "Creates an Opium configuration file at config/opium.yml"
+      
+      def create_config_file
+        copy_file 'config.yml', File.join( 'config', 'opium.yml' )
+      end
+    end
+  end
+end
