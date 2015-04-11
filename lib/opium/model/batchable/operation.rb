854f738f166fcd16fa09807738d3f09b173ac05a
Joshua Bowers <joshua.bowers+code@gmail.com>
2015-04-10 18:31:22 -0700

    #19: Have fleshed out the batch Operation class, which captures information related to a batch operation. This information will eventually be sent to parse, transformed via the to_parse method.

--- lib/opium/model/batchable/operation.rb
+++ lib/opium/model/batchable/operation.rb
@@ -2,6 +2,31 @@ module Opium
   module Model
     module Batchable
       class Operation
+        def initialize( attributes = {} )
+          validate_key_present( attributes, :method )
+          validate_key_present( attributes, :path )
+          attributes.each do |key, value|
+            send( :"#{key}=", value )
+          end
+        end
+        
+        attr_reader :method, :path, :body
+        
+        def to_parse
+          {
+            method: method.to_s.upcase,
+            path: path
+          }.tap {|result| result[:body] = body if body }
+        end
+        
+        private
+        
+        def validate_key_present( attributes, key )
+          as_symbol, as_string = key.to_sym, key.to_s
+          fail ArgumentError, "missing an operation #{ key } parameter" unless attributes.key?( as_symbol ) || attributes.key?( as_string )
+        end
+        
+        attr_writer :method, :path, :body
       end
     end
   end
--- spec/opium/model/batchable/operation_spec.rb
+++ spec/opium/model/batchable/operation_spec.rb
@@ -1,4 +1,73 @@
 require 'spec_helper'
 
 describe Opium::Model::Batchable::Operation do
+  let(:update_path) { '/1/classes/Game/abcd1234' }
+  let(:body) { { title: 'Skyrim' } }
+  
+  describe '#initialize' do
+    let(:result) { described_class.new( properties ) }
+    
+    context 'with all properties' do
+      let(:properties) { { method: :put, path: update_path, body: body } }
+    
+      it { expect { result }.to_not raise_exception }
+      it 'sets the method' do
+        expect( result.method ).to eq :put
+      end
+      
+      it 'sets the path' do
+        expect( result.path ).to eq update_path
+      end
+      
+      it 'sets the body' do
+        expect( result.body ).to eq body
+      end
+    end
+    
+    context 'when :method is missing' do
+      let(:properties) { { path: update_path, body: body } }
+      
+      it { expect { result }.to raise_exception }
+    end
+    
+    context 'when :path is missing' do
+      let(:properties) { { method: :put, body: body } }
+      
+      it { expect { result }.to raise_exception }
+    end
+    
+    context 'when :body is missing' do
+      let(:properties) { { method: :put, path: update_path } }
+      
+      it { expect { result }.to_not raise_exception }
+      it { expect( result.body ).to be_nil }
+    end
+  end
+  
+  describe '#to_parse' do
+    let(:result) { subject.to_parse }
+    
+    context 'when a body is present' do
+      subject { described_class.new( method: :put, path: update_path, body: body ) }
+      
+      it { expect { result }.to_not raise_exception }
+      it { expect( result ).to be_a Hash }
+      it { expect( result.keys ).to include( :method, :path, :body ) }
+      it 'stringifies and upcases the method' do
+        expect( result[:method] ).to eq 'PUT' 
+      end
+    end
+    
+    context 'without a body' do
+      subject { described_class.new( method: :put, path: update_path ) }
+      
+      it { expect { result }.to_not raise_exception }
+      it { expect( result ).to be_a Hash }
+      it { expect( result.keys ).to include( :method, :path ) }
+      it { expect( result ).to_not have_key( :body ) }
+      it 'stringifies and upcases the method' do
+        expect( result[:method] ).to eq 'PUT' 
+      end
+    end
+  end
 end
