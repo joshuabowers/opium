ae7483f749a8d05cdfd2065d946ed9e050e755df
Joshua Bowers <joshua.bowers+code@gmail.com>
2016-10-06 14:35:58 -0700

    Slight rewrite of the config spec to make it less shouldy.

--- spec/opium/config_spec.rb
+++ spec/opium/config_spec.rb
@@ -1,60 +1,64 @@
 require 'spec_helper'
 
 describe Opium do
-  it { should respond_to(:configure, :config) }
-  it { should respond_to(:load!).with(2).arguments }
-  it { should respond_to(:reset) }
-  
-  describe ':configure' do
+  it { is_expected.to respond_to( :configure, :config ) }
+  it { is_expected.to respond_to( :load! ).with(2).arguments }
+  it { is_expected.to respond_to( :reset ) }
+
+  describe '.configure' do
     it do
       expect {|b| Opium.configure(&b) }.to yield_with_args
     end
   end
-  
-  describe ':config' do
+
+  describe '.config' do
     subject { Opium.config }
-    
-    it { should_not be_nil }
-    it { should be_an( Opium::Config ) }
+
+    it { is_expected.to_not be_nil }
+    it { is_expected.to be_an( Opium::Config ) }
   end
-  
-  describe ':reset' do
+
+  describe '.reset' do
     after { described_class.config.log_network_responses = false }
-    
+
     it 'should put all changed settings back to their defaults' do
       expect { described_class.config.log_network_responses = true }.to change( described_class.config, :log_network_responses ).from( false ).to( true )
       described_class.reset
       described_class.config.log_network_responses.should == false
     end
   end
-  
-  describe ':load!' do
+
+  describe '.load!' do
     let(:file) { File.join( File.dirname( __FILE__ ), 'config', 'opium.yml' ) }
-  
+
     before do
       described_class.load!( file, :test )
     end
-    
+
     after do
       described_class.reset
     end
-  
-    it { subject.config.app_id.should == 'abcd1234' }
-    it { subject.config.api_key.should == 'efgh5678' }
-    it { subject.config.master_key.should == '9012ijkl' }
-    it { subject.config.webhook_key.should == 'mnop7654' }
-    it { subject.config.log_network_responses.should == true }
+
+    subject(:config) { described_class.config }
+
+    context 'a valid config file' do
+      it { expect( config.app_id ).to eq 'abcd1234' }
+      it { expect( config.api_key ).to eq 'efgh5678' }
+      it { expect( config.master_key ).to eq '9012ijkl' }
+      it { expect( config.webhook_key ).to eq 'mnop7654' }
+      it { expect( config.log_network_responses ).to eq true }
+    end
   end
-    
+
   describe Opium::Config do
     it { is_expected.to respond_to( :app_id, :api_key, :master_key, :webhook_key, :log_network_responses ) }
-    
-    describe 'defaults' do
-      its(:app_id) { should == 'PARSE_APP_ID' }
-      its(:api_key) { should == 'PARSE_API_KEY' }
-      its(:master_key) { should == 'PARSE_MASTER_KEY' }
-      its(:webhook_key) { should == 'PARSE_WEBHOOK_KEY' }
-      its(:log_network_responses) { should == false }
+
+    context 'a default config' do
+      it { expect( subject.app_id ).to eq 'PARSE_APP_ID' }
+      it { expect( subject.api_key ).to eq 'PARSE_API_KEY' }
+      it { expect( subject.master_key ).to eq 'PARSE_MASTER_KEY' }
+      it { expect( subject.webhook_key ).to eq 'PARSE_WEBHOOK_KEY' }
+      it { expect( subject.log_network_responses ).to eq false }
     end
   end
 end
